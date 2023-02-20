d913815273f9fadd20cd569efaf75c8fc94b3e54
marcik357 <marcik357@gmail.com>
2023-02-20 16:52:27 +0200

    Add: add styles in App.css; Add: add props to component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
-import Counter from './components/Counter';
-import ClassCounter from './components/ClassCounter';
+// import Counter from './components/Counter';
+// import ClassCounter from './components/ClassCounter';
+import './styles/App.css';
+import PostItem from './components/PostItem';
 
 function App() {
-  const [value, setValue] = useState('Text in input')
+  // const [value, setValue] = useState('Text in input')
 
   return (
     <div className="App">
-      <Counter />
+      <PostItem post={{ id: 1, title: "JavaScript", body: "Description" }} />
+      <PostItem post={{ id: 2, title: "JavaScript", body: "Description" }} />
+      <PostItem post={{ id: 3, title: "JavaScript", body: "Description" }} />
+      <PostItem post={{ id: 4, title: "JavaScript", body: "Description" }} />
     </div>
   );
 }
